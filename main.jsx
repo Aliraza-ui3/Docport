2d7557515d3b20f15686c2e353bb438ec7423ed0
Ali Raza <aliraza.khokhar098@gmail.com>
2025-04-08 19:52:51 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <RouterProvider router={router} />
+    </BrowserRouter>
+  </StrictMode>,
+)
